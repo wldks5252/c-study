c481a85bc30071b4d9a72eecdeec109d12c8c837
wldks5252 <63186821+wldks5252@users.noreply.github.com>
2020-04-15 14:51:25 +0900

    c++과제 2 수정

--- c++과제2.cpp
+++ c++과제2.cpp
@@ -9,11 +9,10 @@ int main()
 	cin >> num;
 	cout << "Second number>> ";
 	cin >> ber;
-
+	
 	cout << "===========계산결과==========="<< endl;
 	cout << "" << num << "+" << ber << "= " << (num + ber) << endl;
 	cout << "" << num << "-" << ber << "= " << (num - ber) << endl;
 	cout << "" << num << "*" << ber << "= " << (num * ber) << endl;
-	cout.precision(1); //첫번째 자리까지 소수점을 표현하겠다는 표시
 	cout << "" << num << "/" << ber << "= " << (num / ber) << endl;
 }
